9fea5b26eea5aa2a06c6a269b4f0b6b8b6511dc2
yaoya2618-dot <yaoya2618@tamu.edu>
2024-10-07 21:49:07 -0500

    Add additional movies to seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,7 +18,11 @@ more_movies = [
   {:title => 'Nomadland', :rating => 'R',
     :release_date => '19-Feb-2021'},
   {:title => 'CODA', :rating => 'PG-13',
-    :release_date => '13-Aug-2021'}
+    :release_date => '13-Aug-2021'},
+    # New movies
+  {:title => 'Inception', :rating => 'PG-13', :release_date => '16-Jul-2010' },
+  {:title => 'The Dark Knight', :rating => 'PG-13', :release_date => '18-Jul-2008' },
+  {:title => 'Interstellar', :rating => 'PG-13', :release_date => '7-Nov-2014' }
 ]
 
 more_movies.each do |movie|
